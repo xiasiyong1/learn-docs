40a4f7c5e837b022d24e9a427498b755c1df2acc
夏思勇 <xiasiyong@ones.cn>
2022-09-13 11:12:51 +0800

    feat: add theme config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Hello VuePress",
+  description: "Just playing around",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+    ],
+  },
+};
